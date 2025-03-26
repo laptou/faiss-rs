7f2ba2a79d0cb3bb8dafa5e95bf757754dbf78cf
Ibiyemi Abiodun <ibiyemi@intulon.com>
2025-03-26 18:02:06 -0400

    customize blas and omp linking

--- faiss-sys/build.rs
+++ faiss-sys/build.rs
@@ -1,11 +1,12 @@
 fn main() {
-    #[cfg(feature = "static")]
+    #[cfg(feature = "static-faiss")]
     static_link_faiss();
-    #[cfg(not(feature = "static"))]
+
+    #[cfg(not(feature = "static-faiss"))]
     println!("cargo:rustc-link-lib=faiss_c");
 }
 
-#[cfg(feature = "static")]
+#[cfg(feature = "static-faiss")]
 fn static_link_faiss() {
     use std::{ffi::OsString, path::PathBuf, process::Command};
 
@@ -21,6 +22,18 @@ fn static_link_faiss() {
         .define("BUILD_TESTING", "OFF")
         .profile("RelWithDebInfo")
         .very_verbose(true);
+
+    let blas_link_mode = if cfg!(feature = "static-blas") {
+        "static"
+    } else {
+        "dylib"
+    };
+
+    let omp_link_mode = if cfg!(feature = "static-omp") {
+        "static"
+    } else {
+        "dylib"
+    };
 
     if let Ok(oneapi_root) = std::env::var("ONEAPI_ROOT") {
         println!("using ONEAPI_ROOT={oneapi_root:?}");
@@ -48,19 +61,19 @@ fn static_link_faiss() {
 
         #[cfg(windows)]
         {
-            println!("cargo:rustc-link-lib=mkl_intel_lp64_dll");
-            println!("cargo:rustc-link-lib=mkl_intel_thread_dll");
-            println!("cargo:rustc-link-lib=mkl_core_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_lp64_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_thread_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_core_dll");
         }
 
         #[cfg(unix)]
         {
-            println!("cargo:rustc-link-lib=mkl_intel_lp64");
-            println!("cargo:rustc-link-lib=mkl_intel_thread");
-            println!("cargo:rustc-link-lib=mkl_core");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_lp64");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_thread");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_core");
         }
 
-        println!("cargo:rustc-link-lib=libiomp5md");
+        println!("cargo:rustc-link-lib={omp_link_mode}=libiomp5md");
 
         cfg.env("MKLROOT", &mkl_root);
         cfg.env("LIB", mkl_root.join("lib"));
@@ -79,23 +92,23 @@ fn static_link_faiss() {
 
         #[cfg(windows)]
         {
-            println!("cargo:rustc-link-lib=mkl_intel_lp64_dll");
-            println!("cargo:rustc-link-lib=mkl_intel_thread_dll");
-            println!("cargo:rustc-link-lib=mkl_core_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_lp64_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_thread_dll");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_core_dll");
         }
 
         #[cfg(unix)]
         {
-            println!("cargo:rustc-link-lib=mkl_intel_lp64");
-            println!("cargo:rustc-link-lib=mkl_intel_thread");
-            println!("cargo:rustc-link-lib=mkl_core");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_lp64");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_intel_thread");
+            println!("cargo:rustc-link-lib={blas_link_mode}=mkl_core");
         }
 
-        println!("cargo:rustc-link-lib=dylib=libiomp5md");
+        println!("cargo:rustc-link-lib={omp_link_mode}=libiomp5md");
         cfg.define("MKLROOT", mkl_root);
     } else {
-        println!("cargo:rustc-link-lib=blas");
-        println!("cargo:rustc-link-lib=lapack");
+        println!("cargo:rustc-link-lib={blas_link_mode}=blas");
+        println!("cargo:rustc-link-lib={blas_link_mode}=lapack");
 
         #[cfg(target_os = "macos")]
         {
@@ -124,12 +137,12 @@ fn static_link_faiss() {
                 );
             }
 
-            println!("cargo:rustc-link-lib=dylib=omp");
+            println!("cargo:rustc-link-lib={omp_link_mode}=omp");
         }
 
         #[cfg(not(target_os = "macos"))]
         {
-            println!("cargo:rustc-link-lib=dylib=gomp");
+            println!("cargo:rustc-link-lib={blas_link_mode}=gomp");
         }
     }
 
